ecb6df0fccde0708d2a33987db81b881d2299cbe
sahadz <59341122+sahadz@users.noreply.github.com>
2020-05-24 11:26:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ app.use('/ariang', express.static(__dirname + '/ariang'))
 app.get('/', (req, res) => {
 	res.send(`
 <label for="secret">Enter Password:</label>
-<input id="secret" type="password" placeholder="enter password> <br> <br>
+<input id="secret" type="password" placeholder="enter password"> <br> <br>
 <center><button id="panel">Submit</button>
 <button id="downloads">Downloads</button>
-<button id="clear">clear</button>
+
 <input id="clear" type="reset"></center>
 
 <script>
